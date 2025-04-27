bad2ecb775c09319d7ca157f54eab5b653a93042
Ahmed -El-Seginy <ahmed0elseginy@gmail.com>
2025-04-27 21:50:17 +0300

    ..

--- atc-backend/src/main/java/com/atc/AtcBackendApplication.java
+++ atc-backend/src/main/java/com/atc/AtcBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AtcBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AtcBackendApplication.class, args);
     }
-
 }
